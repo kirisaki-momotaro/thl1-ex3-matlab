7290e608225e9ed3dc10b4323885052f3b7592c8
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-06-04 15:55:01 +0300

    bits_to_PSK_16 done

--- M_PSK.m
+++ M_PSK.m
@@ -10,17 +10,11 @@ bit_seq = (sign(randn(4*N, 1)) + 1)/2;
 bit_seq(1:4)
 X = bits_to_PSK_16(bit_seq); %random symbols
 
-X
 
-uniqueValues = unique(X); % Get unique values
-numUniqueValues = numel(uniqueValues); % Count the number of unique values
-
-disp('Unique Values:');
-disp(uniqueValues);
-disp('Number of Unique Values:');
-disp(numUniqueValues);
-
-
+%PSK ASTERISM
+real=X(1,:)
+imag=X(2,:)
+plot(real,imag,'o')
 
 
 
